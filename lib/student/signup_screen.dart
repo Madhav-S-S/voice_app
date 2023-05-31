a12e1255d3e5707214246ed766c78e5b3487068a
MadhavSS1595 <madhavgs2002@gmail.com>
2023-05-31 16:14:36 +0530

    Roll Number drop button added to sign up signup screen

--- lib/student/signup_screen.dart
+++ lib/student/signup_screen.dart
@@ -31,12 +31,18 @@ class _SignUpState extends State<SignUpScreen> {
   bool _isObscure = true;
   bool _isObscure2 = true;
   File? file;
+  var roll = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','18','19','20','21','22','23','24','25','26','27','28','29','30','31',
+  '32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62',
+  '63','64','65','66','67','68','69','70','71','72','73','74','75','76','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93',
+  '94','95','96','97','98','99','100','101','102','103','104','105','106','108','109','110','111','112','113','114','115','116','117','118','119','120'];
   var options = [
     'Student',
     'Teacher',
   ];
   var _currentItemSelected = "Student";
   var role = "Student";
+  var _currentItemSelected2 = "1";
+  var rollNo = "1";
 
   @override
   Widget build(BuildContext context) {
@@ -204,40 +210,88 @@ class _SignUpState extends State<SignUpScreen> {
                               "Role : ",
                               style: TextStyle(
                                 fontSize: 20,
-                                fontWeight: FontWeight.bold,
                                 color: Colors.white,
+                                fontFamily: 'Poppins'
                               ),
                             ),
-                            DropdownButton<String>(
-                              dropdownColor: Colors.blue[900],
-                              isDense: true,
-                              isExpanded: false,
-                              iconEnabledColor: Colors.white,
-                              focusColor: Colors.white,
-                              items: options.map((String dropDownStringItem) {
-                                return DropdownMenuItem<String>(
-                                  value: dropDownStringItem,
-                                  child: Text(
-                                    dropDownStringItem,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
+                            Container(
+                              child: DropdownButton<String>(
+                                dropdownColor: Colors.blue[900],
+                                isDense: true,
+                                isExpanded: false,
+                                iconEnabledColor: Colors.white,
+                                focusColor: Colors.white,
+                                items: options.map((String dropDownStringItem) {
+                                  return DropdownMenuItem<String>(
+                                    value: dropDownStringItem,
+                                    child: Text(
+                                      dropDownStringItem,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
                                     ),
-                                  ),
-                                );
-                              }).toList(),
-                              onChanged: (newValueSelected) {
-                                setState(() {
-                                  _currentItemSelected = newValueSelected!;
-                                  role = newValueSelected;
-                                });
-                              },
-                              value: _currentItemSelected,
+                                  );
+                                }).toList(),
+                                onChanged: (newValueSelected) {
+                                  setState(() {
+                                    _currentItemSelected = newValueSelected!;
+                                    role = newValueSelected;
+                                  });
+                                },
+                                value: _currentItemSelected,
+                              ),
                             ),
+                            //if _currentItemSelected == "Student" then show the department dropdown
                           ],
                         ),
                         SizedBox(
+                          height: 20,
+                        ),
+                        if (_currentItemSelected == "Student")
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Roll Number : ",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  DropdownButton<String>(
+                                    dropdownColor: Colors.blue[900],
+                                    isDense: true,
+                                    isExpanded: false,
+                                    iconEnabledColor: Colors.white,
+                                    focusColor: Colors.white,
+                                    items: roll.map((String dropDownStringItem) {
+                                      return DropdownMenuItem<String>(
+                                        value: dropDownStringItem,
+                                        child: Text(
+                                          dropDownStringItem,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                    onChanged: (newValueSelected) {
+                                      setState(() {
+                                        _currentItemSelected2 =
+                                            newValueSelected!;
+                                        rollNo = newValueSelected;
+                                      });
+                                    },
+                                    value: _currentItemSelected2,
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
                           height: 20,
                         ),
                         MaterialButton(
