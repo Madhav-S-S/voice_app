ffa81f6270c8713f3dbc382f7226b4525130fdfb
MadhavSS1595 <madhavgs2002@gmail.com>
2023-06-02 01:25:17 +0530

    Status updating function updated, not tested yet

--- lib/faculty/fac_postcard.dart
+++ lib/faculty/fac_postcard.dart
@@ -5,9 +5,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:voice/faculty/update_status_widget.dart';
 import 'package:voice/methods/firestore_methods.dart';
 import 'package:voice/reusable_widgets/reusable_widget.dart';
+import 'package:voice/utils/color_utils.dart';
 class facultyPostCard extends StatefulWidget {
     final snap;
   const facultyPostCard({
@@ -176,8 +176,95 @@ class _facultyPostCardState extends State<facultyPostCard> {
               //show an icon button for faculties to update the status of the complaint
               IconButton(onPressed: (){
                 showDialog(context: context, builder: (context){
-                  return updateStatusWidget();
-                });
+                  var _statusController = TextEditingController();
+                  return Dialog(
+                    backgroundColor: Colors.transparent,
+                   child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: SingleChildScrollView(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            //make the border circular
+                            //make background blur of the container          
+                            color: Colors.black54.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          height: 500,
+                          width: 300,
+                          child: Column(
+                            children: [
+                              SizedBox(height: 20,),
+                              //increase the size of the update status text
+                              Text('Update Status', style: TextStyle(fontSize: 20,fontFamily: 'Poppins'),),
+                              SizedBox(height: 20,),
+                               Padding(
+                                 padding: const EdgeInsets.only(left: 20,),
+                                 child: TextField(
+                                    controller: _statusController,
+                                    maxLines: null,
+                                    decoration: InputDecoration(
+                                        //set the color of of the field as white
+                                        filled: true,
+                                        fillColor: Colors.white.withOpacity(0.1),
+                                        icon:Icon(Icons.update),
+                                        hintText: "Update Status here",
+                                        hintStyle: TextStyle(color: Colors.grey),
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(20),
+                                          borderSide: BorderSide.none,
+                                        )
+                                    ),
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                               ),
+                                SizedBox(height: 10,),
+                               //create a icon button to submit the status
+                                Align(
+                                  widthFactor: 3,
+                                  alignment: Alignment.topRight,
+                                  child: TextButton(
+                                    //set background color of the button as voiceBlue
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: voiceBlue,
+                                    ),
+                                    onPressed: (){
+                                        var postId = widget.snap['postId'].toString();
+                                        FirebaseFirestore.instance.collection('complaints').doc(postId).update({
+                                          'status': FieldValue.arrayUnion([_statusController.text])
+                                        });
+                                    },
+                                    child: Text('SUBMIT',style: TextStyle(color: Colors.white),),
+                                  ),
+                                ),
+                               ListView(
+                                 shrinkWrap: true,
+                                 children: [
+                                   ListTile(
+                                     title: Text('Complaint ID:'),
+                                     subtitle: Text('123456789'),
+                                   ),
+                                   ListTile(
+                                     title: Text('Complaint Type:'),
+                                     subtitle: Text('General'),
+                                   ),
+                                   ListTile(
+                                     title: Text('Complaint Description:'),
+                                     subtitle: Text('This is a general complaint'),
+                                   ),
+                                   ListTile(
+                                     title: Text('Complaint Status:'),
+                                     subtitle: Text('Open'),
+                                   ),
+                                 ],
+                               ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+}
+                );
               },
                icon: const Icon(Icons.update),
                iconSize: 30,
--- lib/faculty/faculty_methods.dart
+++ lib/faculty/faculty_methods.dart
@@ -3,14 +3,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void updateStatus(String status, BuildContext context) {
+void updateStatus(String status) {
   //get the complaint id from the url
-  String complaintId = ModalRoute.of(context)!.settings.arguments.toString();
   //update the status of the complaint in firestore
-  FirebaseFirestore.instance.collection('complaints').doc(complaintId).update({
-    'status': FieldValue.arrayUnion([status])
-  });
+  // FirebaseFirestore.instance.collection('complaints').doc(postId).update({
+  //   'status': FieldValue.arrayUnion([status])
+  // });
   //show a snackbar to the user
-  ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Status updated successfully')));
 }
